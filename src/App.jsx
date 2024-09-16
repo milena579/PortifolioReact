cbeeabe5710c1844c7bf02f5bfa603a0e6bbeaa7
milena579 <milecalegari@gmail.com>
2024-09-16 15:34:49 -0300

    criando a sobre mim

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,68 @@ function App() {
 
             <div className={style.texthome}>
                 <h3>Olá meu nome é</h3>
-                <h3 id="nome"> Milena Calegari Dourado</h3>
+                <h3 className={style.nome}> Milena Calegari Dourado</h3>
                 <input type="button" value="Download CV"/>
+            </div>
+      </section>
+      <section className={style.sobremim} id='sobremim'>
+                    <div className={style.pessoal}>
+                <div className={style.textsobre}>
+                    <h1>Quem sou eu?</h1>
+                    <p> 
+                        Olá meu nome é Milena ou mais conhecida por Mi.
+                        Atualmente tenho 18 anos e sou apaixonada por tecnologia. Minha trajetória nesse universo se deu início no ano de 2021 meio a pandemia, quando tive a oportunidade
+                        de fazer cursos voltado para a área de programação e logo de início descobri que queria que aquela fosse minha profissão, desenvolvedora.  Ao longo desses 3 anos procurei sempre
+                        aprender cada vez mais e melhorar a cada dia, para que futuramente possa me tornar uma profissional influente no mundo da tecnologia.
+                    </p>
+                </div>
+                <img src="../src/assets/picme.png" alt=""/>
+            </div>
+            <h1>Acadêmico</h1>
+            <div className={style.academico}>
+                <div className={style.cardacademico}>
+                    <img src="../src/assets/escola.png" alt=""/>
+                    <ul>
+                        <li>
+                            Ensino Médio Técnico em Eletrônica - CEEP Curitiba
+                        </li>
+                        <li>
+                            Período 2020 - 2023
+                        </li>
+                        <li>
+                            Técnico integrado ao ensino médio
+                        </li>
+                    </ul>
+                    
+                </div>
+                <div className={style.cardacademico}>
+                    <img src="../src/assets/aluno.png" alt=""/>
+                    <ul>
+                        <li>
+                            Técnico em Desenvolvimento de Sistemas - Senai Curitiba
+                        </li>
+                        <li>
+                            Período 2024 - 2025
+                        </li>
+                        <li>
+                            Ensino técnico em parceria com aprendizagem 
+                        </li>
+                    </ul>
+                </div>
+                <div className={style.cardacademico}>
+                    <img src="../src/assets/diplomado.png" alt=""/>
+                    <ul>
+                        <li>
+                            Superior em Análise e Desenvolvimento de Sistemas -  UFPR 
+                        </li>
+                        <li>
+                            Período 2024 - 2026
+                        </li>
+                        <li>
+                            Ensino superior tecnólogo em Desenvolvimento de Sistemas
+                        </li>
+                    </ul>     
+                </div>
             </div>
       </section>
     </main>
