04069e28faf7cb2b8e1cea605f738fe6ef245c06
milena579 <milecalegari@gmail.com>
2024-09-16 14:51:55 -0300

    criando o menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.module.css'
+import style from './App.module.css'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <header className={style.menu} id='menu'>
+        <nav>
+            <h1>&lsaquo;/M&rsaquo;</h1>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#sobremim">Sobre Mim</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projetos">Projetos</a></li>
+                <li><a href="#contatos">Contato</a></li>
+            </ul>
+        </nav>
+    </header>   
     </>
   )
 }
